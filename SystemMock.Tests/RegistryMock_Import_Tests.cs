a0449f7a44da2b624d5f6e81c429ef23786c6ee0
Jozef Izso <jozef.izso@gmail.com>
2014-10-14 22:24:53 +0200

    RegistryMock can import simple .reg file like expressions.

---
+++ SystemMock.Tests/RegistryMock_Import_Tests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SystemMock.Tests
+{
+    [TestFixture]
+    public class RegistryMock_Import_Tests
+    {
+        private RegistryMock registry;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.registry = new RegistryMock();
+        }
+
+        [Test]
+        public void Import_SingleRegistryKey_CreatesRegistryKey()
+        {
+            // Arrange
+
+            // Act
+            this.registry.Import(@"[HKEY_CURRENT_USER\Software\SystemMock\UnitTests]");
+
+            var actualKey = this.registry.CurrentUser.OpenSubKey(@"Software\SystemMock\UnitTests");
+
+            // Assert
+            Assert.IsNotNull(actualKey, @"Registry key 'HKEY_CURRENT_USER\Software\SystemMock\UnitTests' must exists.");
+        }
+
+        [Test]
+        public void Import_SingleRegistryKeyWithValue_CreatesRegistryKey()
+        {
+            // Arrange
+
+            // Act
+            this.registry.Import(@"[HKEY_CURRENT_USER\Software\SystemMock\UnitTests]" + Environment.NewLine +
+                                 @"""Version""=""1""");
+
+            var actualKey = this.registry.CurrentUser.OpenSubKey(@"Software\SystemMock\UnitTests");
+
+            // Assert
+            Assert.IsNotNull(actualKey, @"Registry key 'HKEY_CURRENT_USER\Software\SystemMock\UnitTests' must exists.");
+
+            var actualVersionValue = actualKey.GetValue("Version");
+            Assert.AreEqual("1", actualVersionValue);
+        }
+
+        [Test]
+        public void Import_SingleRegistryKeyWithSeveralValues_CreatesRegistryKey()
+        {
+            // Arrange
+
+            // Act
+            this.registry.Import(@"[HKEY_CURRENT_USER\Software\SystemMock\UnitTests]" + Environment.NewLine +
+                                 @"""Version""=""1.2.0""" + Environment.NewLine +
+                                 @"""Uninstall""=""0""" + Environment.NewLine +
+                                 @"""Guid""=""{A88848E9-7FB0-4591-A957-A1B9D2C3B3EF}""" + Environment.NewLine);
+
+            var actualKey = this.registry.CurrentUser.OpenSubKey(@"Software\SystemMock\UnitTests");
+
+            // Assert
+            Assert.IsNotNull(actualKey, @"Registry key 'HKEY_CURRENT_USER\Software\SystemMock\UnitTests' must exists.");
+            Assert.AreEqual("1.2.0", actualKey.GetValue("Version"));
+            Assert.AreEqual("0", actualKey.GetValue("Uninstall"));
+            Assert.AreEqual("{A88848E9-7FB0-4591-A957-A1B9D2C3B3EF}", actualKey.GetValue("Guid"));
+        }
+
+        [Test]
+        public void Import_SingleRegistryKeyWithDefaultValue_CreatesRegistryKey()
+        {
+            // Arrange
+
+            // Act
+            this.registry.Import(@"[HKEY_CURRENT_USER\Software\SystemMock\UnitTests]" + Environment.NewLine +
+                                 @"@=""Default Value""" + Environment.NewLine);
+
+            var actualKey = this.registry.CurrentUser.OpenSubKey(@"Software\SystemMock\UnitTests");
+
+            // Assert
+            Assert.IsNotNull(actualKey, @"Registry key 'HKEY_CURRENT_USER\Software\SystemMock\UnitTests' must exists.");
+            Assert.AreEqual("Default Value", actualKey.GetValue(null));
+        }
+
+        [Test]
+        public void Import_SingleRegistryKeyWithDefaultValueAndAtCharacter_CreatesRegistryKey()
+        {
+            // Arrange
+
+            // Act
+            this.registry.Import(@"[HKEY_CURRENT_USER\Software\SystemMock\UnitTests]" + Environment.NewLine +
+                                 @"@=""Default Value""" + Environment.NewLine +
+                                 @"""@""=""At Value""" + Environment.NewLine);
+
+            var actualKey = this.registry.CurrentUser.OpenSubKey(@"Software\SystemMock\UnitTests");
+
+            // Assert
+            Assert.IsNotNull(actualKey, @"Registry key 'HKEY_CURRENT_USER\Software\SystemMock\UnitTests' must exists.");
+            Assert.AreEqual("Default Value", actualKey.GetValue(null));
+            Assert.AreEqual("At Value", actualKey.GetValue("@"));
+        }
+    }
+}
---
+++ SystemMock/RegistryMock.Import.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using SystemInterface.Microsoft.Win32;
+
+namespace SystemMock
+{
+    public partial class RegistryMock
+    {
+        public void Import(string registryText)
+        {
+            using (var reader = new StringReader(registryText))
+            {
+                string line;
+                IRegistryKey currentKey = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.StartsWith("["))
+                    {
+                        var registryKey = line.TrimStart('[').TrimEnd(']');
+
+                        currentKey = this.CreateSubKey(registryKey);
+                        continue;
+                    }
+
+                    int separator = line.IndexOf('=');
+                    if (separator > 0)
+                    {
+                        var valueName = line.Substring(0, separator);
+                        var valueValue = line.Substring(separator + 1);
+
+                        if (valueName == "@")
+                        {
+                            valueName = "";
+                        }
+                        valueName = valueName.Trim('"');
+                        valueValue = valueValue.Trim('"');
+
+                        if (currentKey != null)
+                        {
+                            currentKey.SetValue(valueName, valueValue);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
